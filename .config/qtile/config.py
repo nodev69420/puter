a2ee71b0e26eb63243ae8c91aaca2604c2de7d1a
adam <nodev1@proton.me>
2024-07-17 15:32:27 +0100

    updoot

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -8,13 +8,43 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key(
+        [mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right"
+    ),
+    Key(
+        [mod, "shift"],
+        "j",
+        lazy.layout.shuffle_down(),
+        desc="Move window down"
+    ),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key(
+        [mod, "control"],
+        "h",
+        lazy.layout.grow_left(),
+        desc="Grow window to the left"
+    ),
+    Key(
+        [mod, "control"],
+        "l",
+        lazy.layout.grow_right(),
+        desc="Grow window to the right"
+    ),
+    Key(
+        [mod, "control"],
+        "j",
+        lazy.layout.grow_down(),
+        desc="Grow window down"
+    ),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     Key(
@@ -32,17 +62,39 @@ keys = [
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key(
+        [mod],
+        "t",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window"
+    ),
     Key([mod], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "p", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key(
+        [mod],
+        "p",
+        lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"
+    ),
+    Key(
+        [mod, "shift"],
+        "z",
+        lazy.widget["pomodoro"].toggle_active()
+    ),
+    Key(
+        [mod],
+        "z",
+        lazy.widget["pomodoro"].toggle_break()
+    ),
 ]
 for vt in range(1, 8):
     keys.append(
         Key(
             ["control", "mod1"],
             f"f{vt}",
-            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            lazy.core.change_vt(vt).when(
+                func=lambda: qtile.core.name == "wayland"
+            ),
             desc=f"Switch to VT{vt}",
         )
     )
@@ -62,7 +114,9 @@ for i in groups:
                 [mod, "shift"],
                 i.name,
                 lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                desc="Switch to & move focused window to group {}".format(
+                    i.name
+                ),
             ),
         ]
     )
@@ -91,20 +145,40 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 screens = [
     Screen(
+        wallpaper="~/Pictures/pape/pape.jpg",
+        wallpaper_mode="fill",
         top=bar.Bar(
             [
+                widget.Spacer(length=1),
                 widget.CurrentLayoutIcon(),
-                widget.GroupBox(),
+                widget.GroupBox(
+                    highlight_method="line",
+                    highlight_color=[
+                        "000000",
+                        "000000"
+                    ]
+                ),
                 widget.Prompt(),
-                widget.WindowName(),
+                widget.WindowName(
+                    # background="404040"
+                ),
                 widget.Chord(
                     chords_colors={
                         "launch": ("#ff0000", "#ffffff"),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
+                widget.Spacer(length=14),
+                widget.Cmus(
+                    width=256 + 128,
+                    scroll=True,
+                    scroll_delay=0,
+                    scroll_step=1,
+                ),
+                widget.Spacer(length=14),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a [%I:%M:%S]"),
+                widget.Spacer(length=14),
+                widget.Clock(format="%Y-%m-%d %a [%H:%M:%S]"),
             ],
             28,
         ),
@@ -112,8 +186,18 @@ screens = [
 ]
 
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position()
+    ),
+    Drag(
+        [mod],
+        "Button3",
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -134,6 +218,7 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
---
+++ bookmarks.md
@@ -0,0 +1,8 @@
+# Webpages
+- [Vulkan Triangle](https://vulkan-tutorial.com/en/Drawing_a_triangle/Setup/Physical_devices_and_queue_families)
+
+# PDF Bookmarks
+- [Dragon Compiler: Page 27](file:///home/adam/pedofiles/dragon_compilers.pdf)
+
+# YTubes
+- [State Machines](https://www.youtube.com/watch?v=NTEHRjiAY2I)
